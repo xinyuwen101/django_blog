813d961cf889bf4e23508f2bdf4ea97de2672006
Xinyu Wen <me.xinyuwen@gmail.com>
2020-07-01 14:45:32 -0700

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from taggit.managers import TaggableManager
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='published')
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse(
+            'post:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    username = models.CharField(max_length=120)
+    email = models.EmailField(blank=True, null=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.username, self.post)
---
+++ post/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
+
+from .models import Post
+from .forms import CommentForm
+
+
+def post_list(request, tag_slug=None):
+    if tag_slug:
+        tag = Tag.objects.filter(slug=tag_slug).first()
+        all_posts = Post.objects.filter(tags__in=[tag])
+    else:
+        all_posts = Post.objects.all()
+    tag_list = Tag.objects.all()
+    paginator = Paginator(all_posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'tag_list': tag_list
+        }
+    )
+
+
+def post_detail(request, year, month, day, slug):
+    post = Post.objects.filter(
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        slug=slug
+    ).first()
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    tag_list = Tag.objects.all()
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form,
+            'tag_list': tag_list,
+        }
+    )

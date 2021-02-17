f93d04907317efc402664de0101eb02ce770c784
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-16 17:12:55 -0800

    Update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -120,3 +121,5 @@ MEDIA_URL = '/media/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())

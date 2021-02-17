ffbd4ca90daa5a95ea66ad2b9046329fa0843fe2
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-16 16:58:47 -0800

    Update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

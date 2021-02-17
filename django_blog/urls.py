315b8d837252fbf7f6b417d408020dfc9f4b023d
Xinyu Wen <me.xinyuwen@gmail.com>
2021-02-16 17:06:12 -0800

    Update

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
